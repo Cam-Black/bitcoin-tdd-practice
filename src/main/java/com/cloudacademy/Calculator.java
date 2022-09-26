b800b1cb75b3e3174db35b3284301f15fdeea912
Cam-Black <cameronablack1998@gmail.com>
2022-09-26 14:53:37 +0100

    Renamed Calculator to ConverterSvc and moved within correct package to match test Class file.

--- src/main/java/com/cloudacademy/Calculator.java
+++
@@ -1,7 +0,0 @@
-package com.cloudacademy;
-
-public class Calculator {
-	public int add(int numOne, int numTwo) {
-		return numOne + numTwo;
-	}
-}
---
+++ src/main/java/com/cloudacademy/bitcoin/ConverterSvc.java
@@ -0,0 +1,9 @@
+package com.cloudacademy.bitcoin;
+
+public class ConverterSvc {
+	public ConverterSvc() {
+		super();
+	}
+	
+	
+}
