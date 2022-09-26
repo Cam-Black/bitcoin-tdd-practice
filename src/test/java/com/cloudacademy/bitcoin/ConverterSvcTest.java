bf22ef5c243d1eefa1adb511e4acc387d59a894a
Cam-Black <cameronablack1998@gmail.com>
2022-09-26 15:10:34 +0100

    Created test to get the exchange rate of GBP.

--- src/test/java/com/cloudacademy/bitcoin/ConverterSvcTest.java
+++ src/test/java/com/cloudacademy/bitcoin/ConverterSvcTest.java
@@ -13,8 +13,22 @@ public class ConverterSvcTest {
 		
 		//Act
 		double actual = converterSvc.getExchangeRate("USD");
+		
 		//Assert
 		double expected = 100;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getExchangeRateGBPReturnsGBPExchangeRate() {
+		//Arrange
+		ConverterSvc converterSvc = new ConverterSvc();
+		
+		//Act
+		double actual = converterSvc.getExchangeRate("GBP");
+		
+		//Assert
+		double expected = 200;
 		assertEquals(expected, actual);
 	}
 }
