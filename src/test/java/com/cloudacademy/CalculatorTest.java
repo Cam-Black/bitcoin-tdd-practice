9a7919e3a86201aec02b3b635988c9b1f83b787a
Cam-Black <cameronablack1998@gmail.com>
2022-09-26 11:18:02 +0100

    Created a failing test to check if a calculator method returns expected results.

---
+++ src/test/java/com/cloudacademy/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.cloudacademy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+	Calculator calc;
+	
+	@BeforeEach
+	public void init() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void add() {
+		assertEquals(2, calc.add(1, 1));
+	}
+}
