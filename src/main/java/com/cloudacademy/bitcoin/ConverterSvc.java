8822b68bd26a8323c0e985ca024a0e47abc250b8
Cam-Black <cameronablack1998@gmail.com>
2022-09-27 09:17:30 +0100

    Changed getExchangeRate to get the live exchange rate from an API, and added HTTP Components as a dependency needed to access http calls.

--- src/main/java/com/cloudacademy/bitcoin/ConverterSvc.java
+++ src/main/java/com/cloudacademy/bitcoin/ConverterSvc.java
@@ -1,19 +1,47 @@
 package com.cloudacademy.bitcoin;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+
 public class ConverterSvc {
+	
+	private final String BITCOIN_CURRPRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+	private final HttpGet httpGet = new HttpGet(BITCOIN_CURRPRICE_URL);
+	
+	private CloseableHttpClient httpClient;
+	
 	public ConverterSvc() {
 		super();
+		this.httpClient = HttpClients.createDefault();
 	}
 	
 	public double getExchangeRate(String currency) {
-		if (currency.equalsIgnoreCase("usd")) {
-			return 100;
-		} else if (currency.equalsIgnoreCase("gbp")) {
-			return 200;
-		} else if (currency.equalsIgnoreCase("eur")) {
-			return 300;
+		double rate = 0;
+		try {
+			CloseableHttpResponse response = this.httpClient.execute(httpGet);
+			
+			InputStream inputStream = response.getEntity().getContent();
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+			
+			JsonObject jsonObject = JsonParser.parseReader(br).getAsJsonObject();
+			String n =
+					jsonObject.get("bpi").getAsJsonObject().get(currency).getAsJsonObject().get("rate").getAsString();
+			NumberFormat nf = NumberFormat.getInstance();
+			rate = nf.parse(n).doubleValue();
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
-		return 0;
+		return rate;
 	}
 	
 	public double convertBitcoins(String currency, double coins) {
